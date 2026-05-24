7851f3cd4cfd24fcc2afc1da15ade10a864dcbb6
Edson00376920 <00376920@uca.edu.sv>
2026-05-23 22:41:46 -0600

    Add: archivos y codigo base

---
+++ Laboratorio-3/src/main/java/com/example/laboratorio3/common/mappers/SpecimenMapper.java
@@ -0,0 +1,41 @@
+import com.example.laboratorio3.domain.dto.request.CreateSpecimenRequest;
+import com.example.laboratorio3.domain.dto.request.UpdateSpecimenRequest;
+import com.example.laboratorio3.domain.dto.response.SpecimenResponse;
+
+import java.util.UUID;
+
+@Component
+public class SpecimenMapper {
+
+    public Specimen toEntityCreate(CreateSpecimenRequest request) {
+        return Specimen.builder()
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public Specimen toEntityUpdate(UpdateSpecimenRequest request, UUID id) {
+        return Specimen.builder()
+                .id(id)
+                .name(request.getName())
+                .region(request.getRegion())
+                .dangerLevel(request.getDangerLevel())
+                .isFriendly(request.getIsFriendly())
+                .build();
+    }
+
+    public SpecimenResponse toDto(Specimen specimen) {
+        return SpecimenResponse.builder()
+                .id(specimen.getId())
+                .name(specimen.getName())
+                .region(specimen.getRegion())
+                .dangerLevel(specimen.getDangerLevel())
+                .isFriendly(specimen.getIsFriendly())
+                .build();
+    }
+
+    // TODO: El estudiante deberá agregar aquí el método para mapear un Page<Specimen> a Page<SpecimenResponse>
+    // pista: utilizando .map(this::toDto)
+}
